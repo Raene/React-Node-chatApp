a769effb496bde92da51debbecf7c1e098cbae8c
Raene <crofty.52.bu@gmail.com>
2020-01-01 11:04:33 +0100

    set up socket event "join" set up socket event "disconnect"

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const socketio = require("socket.io");
 const http = require("http");
 const routes = require("./router");
+const { addUser, removeUser, getUser, getUsersInRoom } = require("./users.js");
 const PORT = process.env.PORT || 5000;
 
 const app = express();
@@ -9,10 +10,27 @@ const server = http.createServer(app);
 const io = socketio(server);
 
 io.on("connection", socket => {
-  console.log("We have a new conncection");
+  console.log("We have a new connection");
+  socket.on("join", ({ name, room }, callback) => {
+    const { error, user } = addUser({ id: socket.id, name, room });
+
+    if (error) return callback(error);
+
+    socket.emit("message", {
+      users: "admin",
+      text: `${user.name}, welcome to the room ${user.room}`
+    });
+
+    socket.broadcast
+      .to(user.room)
+      .emit("message", { user: "admin", text: `${user.name}, has joined` });
+    socket.join(user.room);
+
+    callback();
+  });
 
   socket.on("disconnect", () => {
-    console.log("User has left")
+    console.log("User has left");
   });
 });
 
